dc13644fba000193560e2cd1f45b594ea39180f7
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-01-28 23:41:16 +0530

    28 Jan

--- hacker-earth/basics of i/o/lifeunievery.cpp
+++ hacker-earth/basics of i/o/lifeunievery.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 int main(){
-    int n;
-    while(1)
+    int n; //to enter a number of one or 2 digits
+    while(1)  //infinite loop
     {
         cin>>n;
         if(n!=42) cout<<n<<endl;
