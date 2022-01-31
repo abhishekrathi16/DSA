3f1d2f0e565bbcd4d7988e8c14fa4f75b897914d
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-02-01 01:00:35 +0530

    1 Feb

--- Abdul Bari DSA/DSA programs/LL.c
+++ Abdul Bari DSA/DSA programs/LL.c
@@ -1,222 +1,421 @@
-//lec 175
-//program to display a linked list
+// lec 175
+// program to display a linked list
 
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Node{
+struct Node
+{
     int data;
     struct Node *next;
-}*first;
+} *first = NULL, *second = NULL, *third = NULL;
 
-void create(int A[],int n){
+void create(int A[], int n)
+{
     int i;
-    struct Node *t,*last;
-    first=(struct Node*)malloc(sizeof(struct Node));
-    first->data=A[0];
-    first->next=NULL;
-    last=first;
-    for(i=1;i<n;i++){
-        t=(struct Node*)malloc(sizeof(struct Node));
-        t->data=A[i];
-        t->next=NULL;
-        last->next=t;
-        last=t;
+    struct Node *t, *last;
+    first = (struct Node *)malloc(sizeof(struct Node));
+    first->data = A[0];
+    first->next = NULL;
+    last = first;
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
     }
 }
 
-void display(struct Node *p){
-    while(p!=NULL){
-        printf("%d ",p->data);
-        p=p->next;
+void display(struct Node *p)
+{
+    while (p != NULL)
+    {
+        printf("%d ", p->data);
+        p = p->next;
     }
 }
 
-void rdisplay(struct Node *p){
-    if(p!=NULL){
-        printf("%d ",p->data);
+void rdisplay(struct Node *p)
+{
+    if (p != NULL)
+    {
+        printf("%d ", p->data);
         rdisplay(p->next);
     }
 }
 
-int count(struct Node *p){
-    int l=0;
-    while(p){
+int count(struct Node *p)
+{
+    int l = 0;
+    while (p)
+    {
         l++;
-        p=p->next;
+        p = p->next;
     }
     return l;
 }
 
-int rcount(struct Node* p){
-    if(p!=NULL){
-        return rcount(p->next)+1;
+int rcount(struct Node *p)
+{
+    if (p != NULL)
+    {
+        return rcount(p->next) + 1;
     }
-    else{
+    else
+    {
         return 0;
     }
 }
 
-int sum(struct Node* p){
-    int sum=0;
-    while(p){
-        sum+=p->data;
-        p=p->next;
+int sum(struct Node *p)
+{
+    int sum = 0;
+    while (p)
+    {
+        sum += p->data;
+        p = p->next;
     }
     return sum;
 }
 
-int Rsum(struct Node* p){
-    if(p==NULL){
+int Rsum(struct Node *p)
+{
+    if (p == NULL)
+    {
         return 0;
-    }else{
-        return Rsum(p->next)+p->data;
+    }
+    else
+    {
+        return Rsum(p->next) + p->data;
     }
 }
 
-int Max(struct Node *p){
-    int max=-32768;  //can be replaced with INT32_MIN
-    while(p){
-        if(p->data > max){
-            max=p->data;
+int Max(struct Node *p)
+{
+    int max = -32768; // can be replaced with INT32_MIN
+    while (p)
+    {
+        if (p->data > max)
+        {
+            max = p->data;
         }
-        p=p->next;
+        p = p->next;
     }
     return max;
 }
 
-int rmax(struct Node* p){
-    int x=0;
-    if(p==0){
-        return -32768;  //can be replaced with INT32_MIN
+int rmax(struct Node *p)
+{
+    int x = 0;
+    if (p == 0)
+    {
+        return -32768; // can be replaced with INT32_MIN
     }
-    x=rmax(p->next);
-    if(x > p->data){
+    x = rmax(p->next);
+    if (x > p->data)
+    {
         return x;
-    }else{
+    }
+    else
+    {
         return p->data;
     }
 }
 
-struct Node* Lsearch(struct Node *p,int key){
-    while(p){
-        if(key==p->data){
+struct Node *Lsearch(struct Node *p, int key)
+{
+    while (p)
+    {
+        if (key == p->data)
+        {
             return p;
         }
-        p=p->next;
+        p = p->next;
     }
     return NULL;
 }
 
-struct Node* RLsearch(struct Node* p,int key){
-    if(p==NULL){
+struct Node *RLsearch(struct Node *p, int key)
+{
+    if (p == NULL)
+    {
         return NULL;
     }
-    if(key==p->data){
+    if (key == p->data)
+    {
         return p;
     }
-    return RLsearch(p->next,key);
+    return RLsearch(p->next, key);
 }
 
-//improving linear search by Move to head method
-struct Node* ImpLsearch(struct Node *p,int key){
-    struct Node* q=NULL;
-    while(p){
-        if(key==p->data){
-            q->next=p->next;
-            p->next=first;
-            first=p;
+// improving linear search by Move to head method
+struct Node *ImpLsearch(struct Node *p, int key)
+{
+    struct Node *q = NULL;
+    while (p)
+    {
+        if (key == p->data)
+        {
+            q->next = p->next;
+            p->next = first;
+            first = p;
             return p;
         }
-        q=p;
-        p=p->next;
+        q = p;
+        p = p->next;
     }
     return NULL;
 }
 
-void Insert(struct Node* p,int index,int x){
+void Insert(struct Node *p, int index, int x)
+{
     struct Node *t;
-    if(index<0 || index > count(p)){
+    if (index < 0 || index > count(p))
+    {
         return;
     }
-    t=(struct Node*)malloc(sizeof(struct Node));
-    t->data=x;
-    if(index==0){
-        t->next=first;
-        first=t;
-    }else{
-        for(int i=0;i<index-1;i++){
-            p=p->next;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = x;
+    if (index == 0)
+    {
+        t->next = first;
+        first = t;
+    }
+    else
+    {
+        for (int i = 0; i < index - 1; i++)
+        {
+            p = p->next;
         }
-        t->next=p->next;
-        p->next=t;
+        t->next = p->next;
+        p->next = t;
     }
 }
 
-void SortedInsert(struct Node* p,int x){
-    struct Node *t,*q=NULL;
-    t=(struct Node*)malloc(sizeof(struct Node));
-    t->data=x;
-    t->next=NULL;
-    //first we check if there are no nodes at all
-    if(first==NULL){
-        first=t;
-    }else{
-        while(p && p->data < x){
-            q=p;
-            p=p->next;
+void SortedInsert(struct Node *p, int x)
+{
+    struct Node *t, *q = NULL;
+    t = (struct Node *)malloc(sizeof(struct Node));
+    t->data = x;
+    t->next = NULL;
+    // first we check if there are no nodes at all
+    if (first == NULL)
+    {
+        first = t;
+    }
+    else
+    {
+        while (p && p->data < x)
+        {
+            q = p;
+            p = p->next;
         }
-        //here we check if the node inserted is less than the data in first node itself
-        if(p==first){
-            t->next=first;
-            first=t;
-        }else{
-            t->next=q->next;
-            q->next=t;
+        // here we check if the node inserted is less than the data in first node itself
+        if (p == first)
+        {
+            t->next = first;
+            first = t;
+        }
+        else
+        {
+            t->next = q->next;
+            q->next = t;
         }
     }
 }
 
-int Delete(struct Node *p,int index){
+int Delete(struct Node *p, int index)
+{
     struct Node *q;
-    int x=-1,i;
-    if(index<1 || index>count(p)){
+    int x = -1, i;
+    if (index < 1 || index > count(p))
+    {
         return -1;
     }
-    if(index==1){
-        q=first;
-        x=first->data;
-        first=first->next;
+    if (index == 1)
+    {
+        q = first;
+        x = first->data;
+        first = first->next;
         free(q);
         return x;
-    }else{
-        for(i=0;i<index-1;i++){
-            q=p;
-            p=p->next;
+    }
+    else
+    {
+        for (i = 0; i < index - 1; i++)
+        {
+            q = p;
+            p = p->next;
         }
-        q->next=p->next;
-        x=p->data;
+        q->next = p->next;
+        x = p->data;
         free(p);
         return x;
     }
 }
 
-int isSorted(struct Node* p){
-    int x=-65536;
-    while(p){
-        if(p->data < x){
+int isSorted(struct Node *p)
+{
+    int x = -65536;
+    while (p)
+    {
+        if (p->data < x)
+        {
             return -1;
-        }else{
-            x=p->data;
-            p=p->next;
+        }
+        else
+        {
+            x = p->data;
+            p = p->next;
         }
     }
     return 1;
 }
 
-int main(){
-    int A[]={3,5,7,10,15};
-    create(A,5);
+void RemoveDuplicates(struct Node *p)
+{
+    struct Node *q = p->next;
+    while (q != NULL)
+    {
+        if (p->data != q->data)
+        {
+            p = q;
+            q = q->next;
+        }
+        else
+        {
+            p->next = q->next;
+            free(q);
+            q = p->next;
+        }
+    }
+}
+
+void Reverse1(struct Node *p)
+{
+    int *A, i;
+    struct Node *q = p;
+    A = (int *)malloc(sizeof(int) * count(p));
+    while (q != NULL)
+    {
+        A[i] = q->data;
+        q = q->next;
+        i++;
+    }
+    q = p;
+    i--;
+    while (q != NULL)
+    {
+        q->data = A[i];
+        q = q->next;
+        i--;
+    }
+}
+
+void Reverse2(struct Node *p)
+{
+    struct Node *q = NULL, *r = NULL;
+    while (p != NULL)
+    {
+        r = q;
+        q = p;
+        p = p->next;
+        q->next = r;
+    }
+    first = q;
+}
+
+void Reverse3(struct Node *q, struct Node *p)
+{
+    if (p)
+    {
+        Reverse3(p, p->next);
+        p->next = q;
+    }
+    else
+    {
+        first = q;
+    }
+}
+// to create 2nd list
+void create2(int A[], int n)
+{
+    int i;
+    struct Node *t, *last;
+    second = (struct Node *)malloc(sizeof(struct Node));
+    second->data = A[0];
+    second->next = NULL;
+    last = second;
+    for (i = 1; i < n; i++)
+    {
+        t = (struct Node *)malloc(sizeof(struct Node));
+        t->data = A[i];
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void Concat(struct Node *p, struct Node *q)
+{
+    third = p;
+    while (p->next != NULL)
+    {
+        p = p->next;
+    }
+    p->next = q;
+}
+
+void Merge(struct Node *p, struct Node *q)
+{
+    struct Node *last;
+    if (p->data < q->data)
+    {
+        third = last = p;
+        p = p->next;
+        third->next = NULL;
+    }
+    else
+    {
+        third = last = q;
+        q = q->next;
+        third->next = NULL;
+    }
+    while (p != NULL && q != NULL)
+    {
+        if (first->data < second->data)
+        {
+            last->next = p;
+            last = p;
+            p = p->next;
+            last->next = NULL;
+        }
+        else
+        {
+            last->next = q;
+            last = q;
+            q = q->next;
+            last->next = NULL;
+        }
+    }
+    if (p != NULL)
+    {
+        last->next = p;
+    }
+    if(q != NULL)
+    {
+        last->next = q;
+    }
+}
+
+int main()
+{
+    int A[] = {10, 20, 30, 40, 50};
+    create(A, 5);
+    int B[] = {5, 15, 25, 35, 45};
+    create2(B, 5);
     // rdisplay(first);
     // printf("\nLength is: %d\n",rcount(first));
     // printf("Sum is: %d\n",Rsum(first));
@@ -230,8 +429,16 @@ int main(){
     // }
     // Insert(first,3,12);
     // SortedInsert(first,18);
-    Delete(first,3);
+    // Delete(first,3);
+    // RemoveDuplicates(first);
+    // Reverse3(NULL,first);
+    // Concat(first, second);
+    Merge(first,second);
     display(first);
-
+    printf("\n");
+    display(second);
+    printf("\n");
+    display(third);
+    printf("\n");
     return 0;
 }
