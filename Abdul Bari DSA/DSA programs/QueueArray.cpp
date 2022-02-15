3973cd82a5afb5abcb7bd0dd1358009f3ffe2240
abhishekrathi16 <rathiabhishek53@gmail.com>
2022-02-15 23:31:05 +0530

    15th Feb

---
+++ Abdul Bari DSA/DSA programs/QueueArray.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class Queue{
+    private:
+    int front;
+    int rear;
+    int size;
+    int *Q;
+
+    public:
+    Queue(){front=rear=-1; size=10; Q=new int[size];}
+    Queue(int size){front=rear=-1; this->size=size; Q=new int[this->size];}
+    void enqueue(int x);
+    int dequeue();
+    void Display();
+};
+
+void Queue::enqueue(int x){
+    if(rear==size-1){
+        cout<<"Queue full"<<endl;
+    }else{
+        rear++;
+        Q[rear]=x;
+    }
+}
+
+int Queue::dequeue(){
+    int x=-1;
+    if(front==rear){
+        cout<<"Queue is empty"<<endl;
+    }
+    else{
+        x=Q[front+1];
+        front++;
+    }
+    return x;
+}
+
+void Queue::Display(){
+    int i;
+    for(i=front++;i<=rear;i++){
+        cout<<Q[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+int main(){
+    
+    return 0;
+}
---
+++ hacker-earth/basics of io/bestIndex.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long n,i,j,sum=0,c;
+    cin>>n;
+    long arr[n];
+    for(i=0;i<n;i++){
+        cin>>arr[n];
+    }
+    for(i=0;i<n;i++){
+        j=i;
+        c=i+1;
+        while(c>0){
+            
+        }
+    }
+    return 0;
+}
